69b23434833e3736344f1bd70419d125703a3243
origin <2206017@naver.com>
2022-12-03 14:52:55 +0900

    update momentum background

--- js/background.js
+++ js/background.js
@@ -2,8 +2,13 @@ const images = [ "0.jpg", "1.jpg", "2.jpg", "3.jpg" ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+//const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+//bgImage.src = `img/${chosenImage}`;
+src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+//document.body.appendChild(bgImage);
+document.body.style.background = `url(${src}) no-repeat`;
+document.body.style.backgroundSize = "cover";
+document.body.style.width = "100%";
+document.body.style.height = "auto";
